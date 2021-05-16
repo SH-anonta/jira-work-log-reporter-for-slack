bfc42bd53a2bafef860c182dc10fb5d650d50d9d
Sofen Anonta <anonta@outlook.com>
2021-05-16 18:20:34 +0600

    Remove task scheduling related code and package dependency
    Running the script at specific times of weekday will be done by the OS

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 require('dotenv').config({ path: '../.env'});
-const schedule = require('node-schedule');
 const jiraService = require('./services/jira-service');
 const slackService = require('./services/slack-service');
 const logger = require("./utility/logger");
 
-const POST_REPORT_TIME = '0 0 20 * * 1-5';
-
-const scheduledJob = schedule.scheduleJob(POST_REPORT_TIME, function(){
-    reportWorkLogHours().catch(err => {
-        logger.error(err);
-    });
+reportWorkLogHours().catch(err => {
+    logger.error(err);
 });
 
-logger.info('Scheduled job for reporting work log hours.');
+logger.info('Starting work log reporter');
 
 async function  reportWorkLogHours() {
     const mentionUserName = process.env.SLACK_TARGET_USER_NAME;
