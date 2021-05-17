ee9e998d6e300daef2c55ffe2959ff125aa4f8f2
Sofen Anonta <anonta@outlook.com>
2021-05-17 23:36:26 +0600

    Update how slack user is mentioned in message

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ reportWorkLogHours().catch(err => {
 logger.info('Starting work log reporter');
 
 async function  reportWorkLogHours() {
-    const mentionUserName = process.env.SLACK_TARGET_USER_NAME;
+    const mentionUserId = process.env.SLACK_TARGET_USER_MEMBER_ID;
     const totalWorkDuration = await jiraService.getWorkLogDurationToday();
 
-    await slackService.sendMessageToChannel(`@${mentionUserName} has logged ${totalWorkDuration} hours of work today`);
+    await slackService.sendMessageToChannel(`<@${mentionUserId}> has logged ${totalWorkDuration} hours of work today`);
 }
